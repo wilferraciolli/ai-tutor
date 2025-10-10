60028184f05c13a0f1c703693d1d033580a9d2c4
wiliamferraciolli <wiliamferraciolli@gmail.com>
2025-10-10 15:09:36 +0000

    Complete Phase 1, Module 1

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.dev/license
  */
-import {Component} from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 
 @Component({
@@ -15,5 +15,9 @@ import {RouterOutlet} from '@angular/router';
   styleUrl: './app.css',
 })
 export class App {
-  protected title = 'myapp';
+  protected readonly title = signal('My Recipe Box');
+
+  protected selectRecipe(recipe: string) : void {
+    console.log(recipe);
+  }
 }
