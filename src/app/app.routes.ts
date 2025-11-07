44ba0f7787b590aad7c1c319ad7ef32223d13914
wiliamferraciolli <wiliamferraciolli@gmail.com>
2025-11-07 18:54:09 +0000

    Added recipe details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,12 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.dev/license
  */
-import {Routes} from '@angular/router';
+import { Routes } from '@angular/router';
+import { RecipeList } from './recipes/components/recipe-list/recipe-list';
+import { RecipeDetail } from './recipes/components/recipe-detail/recipe-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/recipes', },
+    { path: 'recipes', component: RecipeList },
+    { path: 'recipes/ϊδ', component: RecipeDetail }
+];
---
+++ src/app/recipes/components/recipe-detail/recipe-detail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-detail',
+  imports: [],
+  templateUrl: './recipe-detail.html',
+  styleUrl: './recipe-detail.scss'
+})
+export class RecipeDetail {
+
+}
