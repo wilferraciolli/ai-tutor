2cced9567bd581bcbf1e48f6127da1872773dbfa
wiliamferraciolli <wiliamferraciolli@gmail.com>
2025-11-08 13:56:24 +0000

    added angular styling

--- src/app/recipes/components/recipe-form/recipe-form.ts
+++ src/app/recipes/components/recipe-form/recipe-form.ts
@@ -1,29 +1,40 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal, WritableSignal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { RecipeService } from '../../services/recipe.service';
 import { Router, RouterLink } from '@angular/router';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { merge } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-form',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [ReactiveFormsModule, MatButtonModule, RouterLink],
+  imports: [ReactiveFormsModule, MatButtonModule, MatTooltipModule, MatFormFieldModule, MatInputModule, RouterLink],
   templateUrl: './recipe-form.html',
   styleUrl: './recipe-form.scss'
 })
 export class RecipeForm {
   private readonly _formBuilder: FormBuilder = inject(FormBuilder);
   private readonly _recipeService: RecipeService = inject(RecipeService);
-  private readonly  _routerService = inject(Router);
+  private readonly _routerService = inject(Router);
 
   protected readonly form = this._formBuilder.group({
     name: ['', Validators.required],
     description: ['', [Validators.required, Validators.maxLength(50)]]
   });
 
-  protected disabled: boolean = false;
+  protected errorMessage: WritableSignal<string> = signal('');
 
-  handleSubmit(): void {
+  constructor() {
+    merge(this.form.statusChanges, this.form.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessage());
+  }
+
+  protected handleSubmit(): void {
     console.log(this.form.value);
     this._recipeService.addRecipe({
       id: this._recipeService.findNextId(),
@@ -31,9 +42,17 @@ export class RecipeForm {
       description: this.form.value.description ? this.form.value.description : '',
       imgUrl: '',
       isFavorite: false,
-      ingredients: [] 
+      ingredients: []
     });
 
     this._routerService.navigate(['/']);
+  }
+
+  protected updateErrorMessage(): void {
+    if (this.form.controls.name.hasError('required')) {
+      this.errorMessage.set('You must enter a value');
+    } else {
+      this.errorMessage.set('');
+    }
   }
 }
